23bf7fc13dd2fefa39f3812aab392a040c6eff14
AV.Gogolev <av.gogolev@tensor.ru>
2022-06-22 11:09:04 +0500

    2

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -2,6 +2,14 @@ fun main(args: Array<String>) {
     val name = "Madrigal"
     var healthPoints = 100
 
+//    if (healthPoints == 100){
+//        println("$name is in excellent condition!")
+//    }
+//    else{
+//        println("$name is in awful condition!")
+//    }
+
+
     if (healthPoints == 100) println("$name is in excellent condition!")
     else println("$name is in awful condition!")
 
