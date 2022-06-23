5605cd748c148df98c9ca73d009881178f059ae1
Your Name <you@example.com>
2022-06-23 13:53:10 +0500

    все условия в одну переменную

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -13,26 +13,26 @@ fun main(args: Array<String>) {
         println("NONE")
     }
 
-    if (healthPoints == 100){
-        println("$name is in excellent condition!")
-    }
-    else if (healthPoints >= 90) {
-        println("$name has a few scratches.")
-    }
-    else if (healthPoints >= 75) {
-        if (isBlessed){
-            println("$name has some minor wounds but is healing quite quickly!")
-        } else {
-            println("$name has some minor wounds.")
-        }
-//        println("$name has some minor wounds.")
-    }
-    else if (healthPoints >= 15) {
-        println("$name looks pretty hurt.")
-    }
-    else{
-        println("$name is in awful condition!")
-    }
+//    if (healthPoints == 100){
+//        println("$name is in excellent condition!")
+//    }
+//    else if (healthPoints >= 90) {
+//        println("$name has a few scratches.")
+//    }
+//    else if (healthPoints >= 75) {
+//        if (isBlessed){
+//            println("$name has some minor wounds but is healing quite quickly!")
+//        } else {
+//            println("$name has some minor wounds.")
+//        }
+////        println("$name has some minor wounds.")
+//    }
+//    else if (healthPoints >= 15) {
+//        println("$name looks pretty hurt.")
+//    }
+//    else{
+//        println("$name is in awful condition!")
+//    }
 
 //    if (healthPoints == 100) println("$name is in excellent condition!")
 //    else if (healthPoints >= 90) println("$name has a few scratches.")
@@ -43,5 +43,28 @@ fun main(args: Array<String>) {
 
 //    if (healthPoints == 100) println("$name is in excellent condition!")
 //    else println("$name is in awful condition!")
+
+    val healthStatus = if (healthPoints == 100){
+        "$name is in excellent condition!"
+    }
+    else if (healthPoints >= 90) {
+        "$name has a few scratches."
+    }
+    else if (healthPoints >= 75) {
+        if (isBlessed){
+            "$name has some minor wounds but is healing quite quickly!"
+        } else {
+            "$name has some minor wounds."
+        }
+//        println("$name has some minor wounds.")
+    }
+    else if (healthPoints >= 15) {
+        "$name looks pretty hurt."
+    }
+    else{
+        "$name is in awful condition!"
+    }
+    //Состояние игрока
+    println("$name $healthStatus")
 
 }
