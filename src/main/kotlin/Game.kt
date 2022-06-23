8525e8524d5e2b119631deb48874e58168187ca7
Your Name <you@example.com>
2022-06-23 11:33:46 +0500

    added isImmortal

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -2,6 +2,15 @@ fun main(args: Array<String>) {
     val name = "Madrigal"
     var healthPoints = 89
     val isBlessed = true
+    val isImmortal = false
+
+    //Аура
+    if (isBlessed && healthPoints > 50 || isImmortal){
+        println("GREEN")
+    }
+    else {
+        println("NONE")
+    }
 
     if (healthPoints == 100){
         println("$name is in excellent condition!")
