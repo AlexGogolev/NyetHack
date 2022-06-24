0c406fc31834b506bb8141b89f6750d874513f69
alex <lalala@ya.ru>
2022-06-24 22:30:19 +0500

    added string's pattern

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,21 +1,24 @@
 fun main(args: Array<String>) {
     val name = "Madrigal"
-    var healthPoints = 89
+    var healthPoints = 77
     val isBlessed = true
     val isImmortal = false
 
     //Аура
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    //region old_condition_auraVisible
 //    if (auraVisible){
 //        println("GREEN")
 //    }
 //    else {
 //        println("NONE")
 //    }
+    //endregion old_condition_auraVisible
     val auraColor = if (auraVisible) "GREEN" else "NONE"
-    println(auraColor)
+//    println(auraColor)
 
-//    if (healthPoints == 100){
+    //region Description
+    //    if (healthPoints == 100){
 //        println("$name is in excellent condition!")
 //    }
 //    else if (healthPoints >= 90) {
@@ -57,24 +60,24 @@ fun main(args: Array<String>) {
 //        //else if (healthPoints >= 15) "$name looks pretty hurt."
 //        else if (healthPoints in 15..74) "$name looks pretty hurt."
 //        else "$name is in awful condition!"
+    //endregion
 
     val healthStatus = when (healthPoints){
         100 -> "$name is in excellent condition!"
         in 90..99 -> "$name has a few scratches."
         in 75..89 ->
-            if (isBlessed){
-                "$name has some minor wounds but is healing quite quickly!"
-            }
-            else {
-                "$name has some minor wounds."
-            }
+            if (isBlessed) "$name has some minor wounds but is healing quite quickly!"
+            else "$name has some minor wounds."
         in 15..74 -> "$name looks pretty hurt."
         else -> {"$name is in awful condition!"}
     }
 
     //Состояние игрока
-    println("$name $healthStatus")
+    println("(AuraColor: $auraColor) "+
+            "(Blessed: ${if (isBlessed) "YES" else "NONE"})")
+    println("$healthStatus")
 
+    //region test_when
     val race = "gnome"
     val faction = when (race){
         "dwarf" -> "Keepers of the Mines"
@@ -83,5 +86,5 @@ fun main(args: Array<String>) {
         "human" -> "Free People of the Rolling Hills"
         else -> {}
     }
-
+    //endregion test_when
 }
